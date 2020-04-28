ac001fd63cbe6afd340b5281882590b23c8ac515
Adrian Nita <adi.nita@gmail.com>
2020-04-28 12:21:31 +0300

    Update validation.js

--- validation.js
+++ validation.js
@@ -1,40 +1,40 @@
 let customMessages;
 
 export function init(form, messages) {
-	form.addEventListener('invalid', onInvalid, true);
-	form.addEventListener('input', onInput);
-	customMessages = messages;
+  form.addEventListener('invalid', onInvalid, true);
+  form.addEventListener('input', onInput);
+  customMessages = messages;
 }
 
 function onInvalid(event) {
-	const { target } = event;
-	event.preventDefault();
-	target.form.dataset.submitted = true;
-	if (!target.form.noValidate) {
-		displayMessage(target);
-	}
+  const { target } = event;
+  event.preventDefault();
+  target.form.dataset.submitted = true;
+  if (!target.form.noValidate) {
+    displayMessage(target);
+  }
 }
 
 function onInput(event) {
-	const { form } = event.target;
-	if (!form.noValidate && form.dataset.submitted) {
-		form.checkValidity();
-	}
+  const { form } = event.target;
+  if (!form.noValidate && form.dataset.submitted) {
+    form.checkValidity();
+  }
 }
 
 function displayMessage(element) {
-	const output = element.form.querySelector(`output.error[for="${element.id}"]`);
-	const cause = getCause(element.validity);
-	const message = customMessages?.[element.id]?.[cause];
-	if (output) {
-		output.textContent = message || element.validationMessage;
-	}
+  const output = element.form.querySelector(`output.error[for="${element.id}"]`);
+  const cause = getCause(element.validity);
+  const message = customMessages?.[element.id]?.[cause];
+  if (output) {
+    output.textContent = message || element.validationMessage;
+  }
 }
 
 function getCause(validity) {
-	for (const key in validity) {
-		if (validity[key] === true) {
-			return key;
-		}
-	}
+  for (const key in validity) {
+    if (validity[key] === true) {
+      return key;
+    }
+  }
 }
